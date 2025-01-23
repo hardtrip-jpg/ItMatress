fdbad46a4d9fce517b0c454a90cade597c5a1b96
hardtrip-jpg <user@hardtrip.ca>
2025-01-23 11:38:11 -0500

    figuring out intro sequence
    the goal of the intro sequence is to emulate the feeling of a mario kart race.

--- level_components/intro_sequence.gd
+++ level_components/intro_sequence.gd
@@ -2,8 +2,12 @@ extends Node2D
 class_name IntroSequence
 
 @export var animation_player : AnimationPlayer
+@export var label : Label
+@export var player : Node2D
+
 
 func _ready() -> void:
+	player.process_mode = Node.PROCESS_MODE_DISABLED
 	await get_tree().create_timer(1).timeout
 	SignalManager.start_timer.emit()
-	pass
+	player.process_mode = Node.PROCESS_MODE_INHERIT
